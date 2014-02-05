feacf25b6f8d714dd88ece9239acd000ff42d725
Daniel Bower <daniel.bower@infinum.com>
2014-02-05 14:12:31 -0500

    Update dependent plugins

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,46 +1,28 @@
-grails.project.class.dir = "target/classes"
-grails.project.test.class.dir = "target/test-classes"
-grails.project.test.reports.dir = "target/test-reports"
-grails.project.target.level = 1.6
-//grails.project.war.file = "target/${appName}-${appVersion}.war"
+grails.project.work.dir = 'target'
+grails.project.source.level = 1.6
 
 grails.project.dependency.resolution = {
-    // inherit Grails' default dependencies
-    inherits("global") {
-        // uncomment to disable ehcache
-        // excludes 'ehcache'
-    }
+    
+	inherits("global")
+	
     log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
     repositories {
+        grailsPlugins()
         grailsCentral()
-        // uncomment the below to enable remote dependency resolution
-        // from public Maven repositories
-        //mavenCentral()
-        //mavenLocal()
-        //mavenRepo "http://snapshots.repository.codehaus.org"
-        //mavenRepo "http://repository.codehaus.org"
-        //mavenRepo "http://download.java.net/maven/2/"
-        //mavenRepo "http://repository.jboss.com/maven2/"
+        mavenLocal()
+        mavenCentral()
     }
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
-
-        // runtime 'mysql:mysql-connector-java:5.1.5'
-
-
     }
 
     plugins {
-        build(":tomcat:$grailsVersion",
-              ":release:2.2.1",
-              ":rest-client-builder:1.0.2") {
+        build(":release:2.2.1", ":rest-client-builder:1.0.3") {
             export = false
         }
 
-        runtime(":resources:1.1.6", ":jquery:1.7.1" ){
-            export = false
-        }
-        compile(":twitter-bootstrap:2.0.2.25", ":jquery-ui:1.8.15") {
+        runtime(":resources:1.2", ":jquery:1.10.2.2", ":jquery-ui:1.10.3", 
+				':twitter-bootstrap:3.0.3' ){
             export = false
         }
     }
