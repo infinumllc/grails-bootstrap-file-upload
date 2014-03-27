cdadddadfe564fda1005dbc4cb0e899b88a34d64
Daniel Bower <daniel.d.bower@gmail.com>
2014-03-27 11:42:38 -0400

    Version plugin to 3.0.0

--- BootstrapFileUploadGrailsPlugin.groovy
+++ BootstrapFileUploadGrailsPlugin.groovy
@@ -16,7 +16,7 @@
 
 class BootstrapFileUploadGrailsPlugin {
     // the plugin version
-    def version = "2.1.2"
+    def version = "3.0.0"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.0 > *"
     // the other plugins this plugin depends on
